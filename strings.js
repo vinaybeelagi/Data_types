d633239e67c33b77e86e5831311469b7bad204ca
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-12 19:52:20 +0530

    Task-Check for spam

--- strings.js
+++ strings.js
@@ -1,5 +1,15 @@
 // task-Uppercase the first character
-function ucFirst(str){
-    return str[0].toUpperCase()+str.slice(1)
+// function ucFirst(str){
+//     return str[0].toUpperCase()+str.slice(1)
+// }
+// alert( ucFirst("john") );
+
+//Task-Check for spam
+function checkSpam(str) {
+    let lowerStr = str.toLowerCase();
+    return lowerStr.includes('viagra')|| lowerStr.includes('xxx')
 }
-alert( ucFirst("john") );
+alert( checkSpam('buy ViAgRA now') );
+alert( checkSpam('free xxxxx') );
+alert( checkSpam("innocent rabbit") ); 
+
