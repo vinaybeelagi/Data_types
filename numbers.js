db10eab6276c014c565120e08704ed18045bc62a
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-12 16:54:11 +0530

    task- A random number from min to max

--- numbers.js
+++ numbers.js
@@ -37,3 +37,10 @@ while (i < 11) {
   i += 0.2;
   if (i > 9.8 && i < 10.2) alert( i );
 }
+
+//task- A random number from min to max
+function random(min,max) {
+    return min+Math.random()*(max-min)
+}
+alert(random(1,5));
+alert(random(1,5));
