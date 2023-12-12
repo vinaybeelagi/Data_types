3d88163db07ac36649cd0bc0d72302d5ea1526ee
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-12 13:55:16 +0530

    Task-Why 6.35.toFixed(1) == 6.3?

--- numbers.js
+++ numbers.js
@@ -2,3 +2,10 @@
 let a = +prompt("1st no.","");
 let b = +prompt("2nd no.","");
 alert (a+b);
+
+//Task-Why 6.35.toFixed(1) == 6.3?
+// QUESTION: In the similar example below, why is 6.35 rounded to 6.3, not 6.4?
+
+// alert( 6.35.toFixed(1) ); // 6.3
+
+alert( Math.round(6.35 * 10) / 10 ); // 6.35 -> 63.5 -> 64(rounded) -> 6.4
