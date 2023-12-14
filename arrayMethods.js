89f17ae54ba46a7e622b565959c9be937204f49f
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-14 15:37:15 +0530

    Map to objects

--- arrayMethods.js
+++ arrayMethods.js
@@ -137,10 +137,23 @@
 // console.log(arr1); 
 
 // Task -Map to names:
-let john = { name: "John", age: 25 };
-let pete = { name: "Pete", age: 30 };
-let mary = { name: "Mary", age: 28 };
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let users = [ john, pete, mary ];
+//  let names = users.map(item => item.name);
+// console.log(names);
+
+// Task-Map to objects
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
 
 let users = [ john, pete, mary ];
- let names = users.map(item => item.name);
-console.log(names);
+let usersMapped = users.map(user =>({
+    fullName : `${user.name} ${user.surname}`,
+    id: user.id
+}));
+console.log(usersMapped[0].id);
+console.log(usersMapped[0].fullName);
