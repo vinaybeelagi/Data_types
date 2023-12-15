257341b6c6f8fbf28c7c8813f099d2bff4abcee9
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-15 17:52:12 +0530

    Filter anagrams

--- map_Set.js
+++ map_Set.js
@@ -119,3 +119,20 @@ let values = ["Hare", "Krishna", "Hare", "Krishna",
 ];
 
 console.log( unique(values) );
+
+//Filter anagrams
+function aclean(arr) {
+  let map = new Map();
+
+  for (let word of arr) {
+    // split the word by letters, sort them and join back
+    let sorted = word.toLowerCase().split('').sort().join(''); // (*)
+    map.set(sorted, word);
+  }
+
+  return Array.from(map.values());
+}
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+alert( aclean(arr) );
