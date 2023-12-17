a3fd77777bad6e484d12ec41c917e02caa87137c
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-17 23:23:34 +0530

    array.at()

--- arrays.js
+++ arrays.js
@@ -134,3 +134,17 @@ function sumInput() {
   }
   
   alert( sumInput() );
+
+  // Array.at()
+
+  const array1 = [5, 12, 8, 130, 44];
+
+let index = 2;
+
+console.log(`An index of ${index} returns ${array1.at(index)}`);
+// Expected output: "An index of 2 returns 8"
+
+index = -2;
+
+console.log(`An index of ${index} returns ${array1.at(index)}`);
+// Expected output: "An index of -2 returns 130"
