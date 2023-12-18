ee9d82e4de117e585b9215cf963099f546c5704a
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-18 23:06:51 +0530

    task-  How many seconds till tomorrow?

--- Date_Time.js
+++ Date_Time.js
@@ -53,3 +53,15 @@ function getSecondsToday() {
   }
   
   alert( getSecondsToday() );
+
+//   How many seconds till tomorrow?
+function getSecondsToTomorrow() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let totalSecondsToday = (hour * 60 + minutes) * 60 + seconds;
+    let totalSecondsInADay = 86400;
+  
+    return totalSecondsInADay - totalSecondsToday;
+  }
