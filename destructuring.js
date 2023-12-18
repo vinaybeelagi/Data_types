31b38b7e53855e6d5c62742e817a26e2d4785a67
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-18 16:08:31 +0530

    Array destructuring

---
+++ destructuring.js
@@ -0,0 +1,11 @@
+// Array destructuring
+let arr = ["viraj", "beelagi"]
+let [firstName,surname] = arr;
+console.log(firstName);
+console.log(surname);
+
+let [firstName1,surname1] = "viraj beelagi".split(' ');
+console.log(firstName1);
+console.log(surname1);
+
+
