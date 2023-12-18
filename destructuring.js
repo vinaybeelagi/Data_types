9d4b11c14d033991e781f63ade1037f228c03379
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-18 16:33:16 +0530

    object destructuring

--- destructuring.js
+++ destructuring.js
@@ -8,4 +8,33 @@ let [firstName1,surname1] = "viraj beelagi".split(' ');
 console.log(firstName1);
 console.log(surname1);
 
+const numbers = [1,2,3,4,5];
+const [first,second,third] = numbers;
+console.log(first);
+
+const [one, , three]=numbers;
+console.log(one,three);
+
+//rest operator
+const [head, ...rest] = numbers;
+console.log(head);
+console.log(rest);
+
+// object destructuring
+const person = {name:'aaru',age:4,city:'dwd'};
+const{name,age} =person;
+console.log(name);
+console.log(age);
+
+//varible renmaing
+const{name:personName,age:personAge} =person;
+console.log(personName,personAge);
+
+const{country = 'usa'} = person;
+console.log(country);
+
+const student = {details:{name:'Alice',age:26}};
+const {details:{name:studentName,age:studentAge}}=student;
+console.log(studentName,studentAge);
+
 
