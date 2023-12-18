9e47f64eeb4db73cb0e4fad7850711fafd190767
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-18 22:45:36 +0530

    task-Create a date

---
+++ Date_Time.js
@@ -0,0 +1,10 @@
+// craetion 
+let now = new Date();
+console.log(now);
+
+let date = new Date(2011, 0, 1, 2, 3, 4, 567);
+console.log( date ); // 1.01.2011, 02:03:04.567
+
+// task-Create a date
+let d1 = new Date(2012,1,20,3,12);
+alert(d1);
