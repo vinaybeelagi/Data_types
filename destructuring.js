e4c56b9a986b4798d9698af9ec73009f9c62a6a2
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-18 17:19:23 +0530

    task-Destructuring assignment

--- destructuring.js
+++ destructuring.js
@@ -1,48 +1,66 @@
-// Array destructuring
-let arr = ["viraj", "beelagi"]
-let [firstName,surname] = arr;
-console.log(firstName);
-console.log(surname);
+// // Array destructuring
+// let arr = ["viraj", "beelagi"]
+// let [firstName,surname] = arr;
+// console.log(firstName);
+// console.log(surname);
 
-let [firstName1,surname1] = "viraj beelagi".split(' ');
-console.log(firstName1);
-console.log(surname1);
+// let [firstName1,surname1] = "viraj beelagi".split(' ');
+// console.log(firstName1);
+// console.log(surname1);
 
-const numbers = [1,2,3,4,5];
-const [first,second,third] = numbers;
-console.log(first);
+// const numbers = [1,2,3,4,5];
+// const [first,second,third] = numbers;
+// console.log(first);
 
-const [one, , three]=numbers;
-console.log(one,three);
+// const [one, , three]=numbers;
+// console.log(one,three);
 
-//rest operator
-const [head, ...rest] = numbers;
-console.log(head);
-console.log(rest);
+// //rest operator
+// const [head, ...rest] = numbers;
+// console.log(head);
+// console.log(rest);
 
-// object destructuring
-const person = {name:'aaru',age:4,city:'dwd'};
-const{name,age} =person;
-console.log(name);
-console.log(age);
+// // object destructuring
+// const person = {name:'aaru',age:4,city:'dwd'};
+// const{name,age} =person;
+// console.log(name);
+// console.log(age);
 
-//varible renmaing
-const{name:personName,age:personAge} =person;
-console.log(personName,personAge);
+// //varible renmaing
+// const{name:personName,age:personAge} =person;
+// console.log(personName,personAge);
 
-const{country = 'usa'} = person;
-console.log(country);
+// const{country = 'usa'} = person;
+// console.log(country);
 
-const student = {details:{name:'Alice',age:26}};
-const {details:{name:studentName,age:studentAge}}=student;
-console.log(studentName,studentAge);
+// const student = {details:{name:'Alice',age:26}};
+// const {details:{name:studentName,age:studentAge}}=student;
+// console.log(studentName,studentAge);
 
-//Looping with .entries()
+// //Looping with .entries()
+// let user = {
+//     name : "rohit",
+//     age:38
+// };
+// for(let [key, value] of Object.entries(user)){
+//     console.log(`${key}:${value}`);
+// }
+
+// let user = new Map();
+// user.set("name","dhoni");
+// user.set("age","30");
+// for(let [key ,value] of user){
+//     console.log(`${key}:${value}`);
+// }
+
+// Destructuring assignment
 let user = {
-    name : "rohit",
-    age:38
-};
-for(let [key, value] of Object.entries(user)){
-    console.log(`${key}:${value}`);
-}
+    name: "John",
+    years: 30
+  };
+  let {name, years:age ,isAdmin = false} = user;
+  console.log(name);
+  console.log(age);
+  console.log(isAdmin);
+
 
