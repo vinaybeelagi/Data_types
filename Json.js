46517fb348b5a3d64b68833aa8d60adc2d48b5ab
vinaybeelagi <vinaykumar.b@neoito.com>
2023-12-19 13:14:20 +0530

    Turn the object into JSON and back

--- Json.js
+++ Json.js
@@ -32,3 +32,28 @@ let meetup = {
 
 console.log( JSON.stringify(meetup) );
 
+// The important limitation: there must be no circular references.
+let room = {
+    number: 24
+  };
+  
+  let meetup1 = {
+    title: "Conference",
+    participants: ["henry", "ann"]
+  };
+  
+  meetup1.place = room; 
+  console.log(meetup1)  ;    // meetup references room
+  room.occupiedBy = meetup1; // room references meetup
+  console.log(room);
+  JSON.stringify(meetup); // Error: Converting circular structure to JSON
+
+//   TASK-Turn the object into JSON and back
+let user4 = {
+    name: "John Smith",
+    age: 35
+  };
+  console.log(JSON.stringify(user4));
+  let user5 = JSON.parse(JSON.stringify(user4));
+  console.log(user5);
+
